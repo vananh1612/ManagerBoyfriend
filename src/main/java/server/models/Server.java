aecc86cd925966e5f3b18d5abba01bcafdf54bec
Nam077 <nam077.me@gmail.com>
2023-05-01 04:54:32 +0700

    Thêm Model Server để triển khai RMI

--- src/main/java/server/models/Server.java
+++ src/main/java/server/models/Server.java
@@ -1,4 +1,31 @@
 package server.models;
 
+import server.configs.Configs;
+import server.interfaces.UserManagerInterface;
+import server.managers.UserManager;
+import server.services.database.UserService;
+import shared.models.User;
+
+import java.lang.reflect.Array;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.ArrayList;
+
 public class Server {
+    public UserManagerInterface userManager;
+    public Server() {
+        try {
+            userManager = new UserManager(new UserService());
+            LocateRegistry.createRegistry(Configs.SERVER_PORT);
+            LocateRegistry.getRegistry(8080).rebind("UserManager", userManager);
+            System.out.println("Server created");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        new Server();
+    }
 }
+
