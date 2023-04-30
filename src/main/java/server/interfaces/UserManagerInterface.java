a8206ee89a0825a8a99d1019d1f92cfbeb14efe2
Nam077 <nam077.me@gmail.com>
2023-05-01 04:52:19 +0700

    Thêm SQLException

--- src/main/java/server/interfaces/UserManagerInterface.java
+++ src/main/java/server/interfaces/UserManagerInterface.java
@@ -1,6 +1,7 @@
 package server.interfaces;
 
 import java.rmi.RemoteException;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import shared.models.Notification;
@@ -9,26 +10,26 @@ import shared.models.User;
 public interface UserManagerInterface extends ManagerInterface<User> {
 
     @Override
-    Notification<User> create(User data) throws RemoteException;
+    Notification<User> create(User data) throws RemoteException, SQLException;
 
     @Override
-    Notification<User> update(int id, User data) throws RemoteException;
+    Notification<User> update(int id, User data) throws RemoteException, SQLException;
 
     @Override
-    Notification<User> delete(int id) throws RemoteException;
+    Notification<User> delete(int id) throws RemoteException, SQLException;
 
     @Override
-    ArrayList<User> getAll() throws RemoteException;
+    ArrayList<User> getAll() throws RemoteException, SQLException;
 
     @Override
-    User getById(int id) throws RemoteException;
+    User getById(int id) throws RemoteException, SQLException;
 
     @Override
-    ArrayList<User> getAllBy(String field, String value) throws RemoteException;
+    ArrayList<User> getAllBy(String field, String value) throws RemoteException, SQLException;
 
     @Override
-    User getBy(String field, String value) throws RemoteException;
+    User getBy(String field, String value) throws RemoteException, SQLException;
 
     @Override
-    ArrayList<User> search(String field, String value) throws RemoteException;
+    ArrayList<User> search(String field, String value, boolean findAll) throws RemoteException, SQLException;
 }
