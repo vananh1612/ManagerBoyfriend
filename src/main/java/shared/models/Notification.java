82752eb8673c596f12eec98d59c539190a96cdde
Nam077 <nam077.me@gmail.com>
2023-05-01 03:48:03 +0700

    add constructor

--- src/main/java/shared/models/Notification.java
+++ src/main/java/shared/models/Notification.java
@@ -1,6 +1,8 @@
 package shared.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
@@ -10,14 +12,22 @@ import java.io.Serializable;
  * @param <T> Kiểu dữ liệu liên kết với thông báo
  */
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Notification<T> implements Serializable {
 
-    /** Chuỗi thông báo */
+    /**
+     * Chuỗi thông báo
+     */
     private String message;
 
-    /** Dữ liệu liên quan đến thông báo */
-    private T data;
-
-    /** Trạng thái thành công của thông báo */
+    /**
+     * Trạng thái thành công của thông báo
+     */
     private boolean success;
+
+    /**
+     * Dữ liệu liên quan đến thông báo
+     */
+    private T data;
 }
--- src/main/java/shared/models/User.java
+++ src/main/java/shared/models/User.java
@@ -2,12 +2,16 @@ package shared.models;
 
 import java.io.Serializable;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * Lớp User đại diện cho một người dùng trong hệ thống
  */
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User implements Serializable { 
 
     /** ID của người dùng. */
