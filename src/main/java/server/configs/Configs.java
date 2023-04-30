02c0ae543637f0a42f792bfa48dc72acfde5eaf5
Nam077 <nam077.me@gmail.com>
2023-05-01 04:52:45 +0700

    Thêm Cấu Hình chạy Server

--- src/main/java/server/configs/Configs.java
+++ src/main/java/server/configs/Configs.java
@@ -9,6 +9,9 @@ public class Configs {
     public static final String MSQL_PASSWORD = "12345";
     public static final String MSQL_DATABASE = "score";
 
+    public static final int SERVER_PORT = 8080;
+    public static final String SERVER_NAME = "Score";
+    public static final String SERVER_ADDRESS = "localhost";
     // Config
 
 }
