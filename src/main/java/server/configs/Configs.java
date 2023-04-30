2136e22be1103d8a4b7da90465a96246849c24db
Nam077 <nam077.me@gmail.com>
2023-05-01 01:07:01 +0700

    Init project create structure server client

---
+++ src/main/java/server/configs/Configs.java
@@ -0,0 +1,13 @@
+package server.configs;
+
+public class Configs {
+
+    // Config Mysql
+    public static final String MSQL_HOST = "localhost";
+    public static final String MSQL_PORT = "3306";
+    public static final String MSQL_USER = "root";
+    public static final String MSQL_PASSWORD = "";
+
+    // Config
+
+}
