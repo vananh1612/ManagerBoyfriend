22fa6c10f05757fd03297f7a166ba582c945f9fd
Nam077 <nam077.me@gmail.com>
2023-05-01 04:52:01 +0700

    Thêm dependency injection

--- src/main/java/server/managers/UserManager.java
+++ src/main/java/server/managers/UserManager.java
@@ -1,50 +1,64 @@
 package server.managers;
 
+import server.interfaces.DaoInterface;
 import server.interfaces.UserManagerInterface;
+import server.services.database.UserService;
 import shared.models.Notification;
 import shared.models.User;
 
 import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
-public class UserManager implements UserManagerInterface {
-    @Override
-    public Notification<User> create(User data) throws RemoteException {
-        return null;
+public class UserManager extends UnicastRemoteObject implements UserManagerInterface {
+
+    private DaoInterface<User> userService;
+
+    public UserManager(DaoInterface userService) throws RemoteException {
+        this.userService = userService;
     }
 
     @Override
-    public Notification<User> update(int id, User data) throws RemoteException {
-        return null;
+    public Notification<User> create(User data) throws RemoteException, SQLException {
+        return this.userService.create(data);
     }
 
     @Override
-    public Notification<User> delete(int id) throws RemoteException {
-        return null;
+    public Notification<User> update(int id, User data) throws RemoteException, SQLException {
+        return this.userService.update(id, data);
     }
 
     @Override
-    public ArrayList<User> getAll() throws RemoteException {
-        return null;
+    public Notification<User> delete(int id) throws RemoteException, SQLException {
+        return this.userService.delete(id);
     }
 
     @Override
-    public User getById(int id) throws RemoteException {
-        return null;
+    public ArrayList<User> getAll() throws RemoteException, SQLException {
+        return this.userService.getAll();
     }
 
     @Override
-    public ArrayList<User> getAllBy(String field, String value) throws RemoteException {
-        return null;
+    public User getById(int id) throws RemoteException, SQLException {
+        return this.userService.getById(id);
     }
 
     @Override
-    public User getBy(String field, String value) throws RemoteException {
-        return null;
+    public ArrayList<User> getAllBy(String field, String value) throws RemoteException, SQLException {
+        return this.userService.getAllBy(field, value);
     }
 
     @Override
-    public ArrayList<User> search(String field, String value) throws RemoteException {
-        return null;
+    public User getBy(String field, String value) throws RemoteException, SQLException {
+        return this.userService.getBy(field, value);
     }
+
+
+    @Override
+    public ArrayList<User> search(String field, String value, boolean findAll) throws RemoteException, SQLException {
+        return this.userService.search(field, value, findAll);
+    }
+
+
 }
